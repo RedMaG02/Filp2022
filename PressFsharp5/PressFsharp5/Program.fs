31c6c61da3de1353f6fe26da824c0bdae2912261
RedMaG02 <94241600+RedMaG02@users.noreply.github.com>
2022-03-25 01:50:03 +0300

    5.11

--- PressFsharp5/PressFsharp5/Program.fs
+++ PressFsharp5/PressFsharp5/Program.fs
@@ -1,4 +1,14 @@
-﻿
+﻿open System
 
-// For more information see https://aka.ms/fsharp-console-apps
-printfn "Hello from F#"
+let answer n= 
+    match n with
+    |"f#"| "prolog" -> "Подлиза"
+    |"c++" -> "Ну хоть не f#"
+    |"c#"| "java" -> "Норм чел, объектно-ориентированный..."
+    |_ -> "Не знаю таких..."
+
+[<EntryPoint>]
+let main argv =
+    Console.WriteLine("Любимый язык программирования?")
+    Console.WriteLine(answer (Console.ReadLine()))
+    0
