276d0d8f8c5670ebf0faa46886b368e04b751d0c
RedMaG02 <94241600+RedMaG02@users.noreply.github.com>
2022-03-25 15:03:17 +0300

    12

---
+++ 5.12/5.12/Program.fs
@@ -0,0 +1,21 @@
+﻿open System
+[<EntryPoint>]
+let main argv =
+    let answer n= 
+        match n with
+        |"f#"| "prolog" -> "Подлиза"
+        |"c++" -> "Ну хоть не f#"
+        |"c#"| "java" -> "Норм чел, объектно-ориентированный..."
+        |_ -> "Не знаю таких..."
+    let printx (x:string) =
+        Console.WriteLine(x)
+    let c = 
+        Console.WriteLine("Любимый язык программирования?")
+        Console.ReadLine();
+
+
+    (answer>>printx) c
+
+
+    Console.ReadLine() |> answer |> Console.WriteLine
+    0
