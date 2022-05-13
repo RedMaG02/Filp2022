135af92b36c3059eb87ddf1d3b070e16a8769a0d
RedMaG02 <94241600+RedMaG02@users.noreply.github.com>
2022-05-13 23:27:56 +0300

    14

---
+++ 5.13/5.13/Program.fs
@@ -0,0 +1,20 @@
+﻿open System
+
+let rec mult n =
+    if n = 0 then 1
+    else (n%10) * mult(n/10)
+
+let rec multDel n = 
+    let rec multHvost n acc =
+        if n = 0 then acc
+        else multHvost (n/10) (acc * (n%10))
+    multHvost n 1
+    
+
+[<EntryPoint>]
+let main argv =
+    Console.WriteLine(mult (Convert.ToInt32(Console.ReadLine())))
+    Console.WriteLine(multDel (Convert.ToInt32(Console.ReadLine())))
+    0
+
+    
