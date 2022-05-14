2c172c01610c580c932cc911408c92461ad82e77
RedMaG02 <94241600+RedMaG02@users.noreply.github.com>
2022-05-14 14:54:20 +0300

    5.17.2

--- 5.18/5.18/Program.fs
+++ 5.18/5.18/Program.fs
@@ -25,7 +25,14 @@ let method1 x =
             z15 x func new_init (del-1)
     z15 x (fun x y -> x + 1) 0 x
 
+let method2 x =
+    let rec maks3 x _max =
+        if x = 0 then _max
+        else if (x%10) > _max && (x%10)%3<>0 then maks3 (x/10) (x%10) else  maks3 (x/10) _max
+    maks3 x 0        
+    
 [<EntryPoint>]
 let main argv =
     Console.WriteLine(method1 15)
+    Console.WriteLine(method2 76359 )
     0 // return an integer exit code 14 13 11 8 7 4 2 1 (15 12 10 9 6 5 3 )
