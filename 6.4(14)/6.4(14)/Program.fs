379310695880ea885801005dbdc8f40c31dd6fac
RedMaG02 <94241600+RedMaG02@users.noreply.github.com>
2022-05-15 16:46:35 +0300

    6.4(14)

---
+++ 6.4(14)/6.4(14)/Program.fs
@@ -0,0 +1,39 @@
+﻿// Learn more about F# at http://docs.microsoft.com/dotnet/fsharp
+
+open System
+
+let rec readList n = 
+    if n=0 then []
+    else
+        let Head = Convert.ToInt32(Console.ReadLine())
+        let Tail = readList (n-1)
+        Head::Tail
+
+let rec writeList list= 
+    match list with
+    | [] -> ()
+    | head::tail -> 
+        printfn "%O" head
+        writeList tail
+
+let getAmountBetweenAandB list a b =
+    let rec getAmountBetweenAandBRec list a b count=
+        match list with
+        |[] -> count
+        |head::tail ->
+            if head>a && head<b then getAmountBetweenAandBRec tail a b (count+1)
+            else getAmountBetweenAandBRec tail a b count
+    getAmountBetweenAandBRec list a b 0
+
+[<EntryPoint>]
+let main argv =
+    let a =
+        Convert.ToInt32(Console.ReadLine())
+    let b =
+        Convert.ToInt32(Console.ReadLine())
+    let n =
+        Convert.ToInt32(Console.ReadLine())
+    let list =
+        readList n
+    Console.WriteLine(getAmountBetweenAandB list a b)
+    0
