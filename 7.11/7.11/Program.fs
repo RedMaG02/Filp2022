aa82d78ddf14664b5d6d8530daab73cc9620c0d6
RedMaG02 <94241600+RedMaG02@users.noreply.github.com>
2022-05-16 00:38:17 +0300

    7.11(4)

---
+++ 7.11/7.11/Program.fs
@@ -0,0 +1,30 @@
+﻿// Learn more about F# at http://docs.microsoft.com/dotnet/fsharp
+
+open System
+
+let rec readList n = 
+    if n=0 then []
+    else
+        let Head = Convert.ToInt32(Console.ReadLine())
+        let Tail = readList (n-1)
+        Head::Tail
+
+let rec writeList list= 
+    List.iter (printfn "%d" ) list
+
+// Define a function to construct a message to print
+let method4 list =
+    let pairList = List.indexed list
+    let pairList2 = List.sortByDescending (snd) pairList
+    List.map fst pairList2
+
+[<EntryPoint>]
+let main argv =
+    let n = 
+        Convert.ToInt32(Console.ReadLine())
+    let list = 
+        readList n
+    let newList =
+        method4 list
+    writeList newList
+    0 // return an integer exit code
