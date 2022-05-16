ec754de8024fc62e1cc825808f7eb8145bdbdcfb
RedMaG02 <94241600+RedMaG02@users.noreply.github.com>
2022-05-16 18:13:26 +0300

    7.20.2(7)

--- 7.20(4,7)/7.20(4,7)/Program.fs
+++ 7.20(4,7)/7.20(4,7)/Program.fs
@@ -27,7 +27,32 @@ let sortByAverageASCII (strings:'string list) =
     let sortedList = List.sortBy (fun x-> pown ((getAverageStringWeight x) - firstStringWeight) 2) strings.Tail
     sortedList
 
+
+
+
+let isGlasn (x : char) =
+    (x = 'а') || (x = 'и') || (x = 'е') || (x = 'ё') || (x = 'о') || (x = 'у') || (x = 'ы') || (x = 'э') || (x = 'ю') || (x = 'я')
+
+let counts x y =
+    if (isGlasn (snd y)) && (not (isGlasn (fst y))) then x + 1
+        else if (isGlasn (fst y)) && (not(isGlasn(snd y))) then x - 1
+                else x
+
+let countSoch str =
+    let unList = Seq.pairwise str
+    Seq.fold counts 0 unList
+
+let sortBySoch str_list : string list =
+    Seq.toList (Seq.sortBy countSoch str_list)
+
+
 [<EntryPoint>]
 let main argv =
-
+    let n = 
+        Convert.ToInt32(Console.ReadLine())
+    let stringList = 
+        readStringList n
+    let newList =
+        sortBySoch stringList
+    writeList newList
     0 // return an integer exit code
