45d63854d3e8e87014ea21bec52764d82bda78cb
RedMaG02 <94241600+RedMaG02@users.noreply.github.com>
2022-05-16 18:19:06 +0300

    7.20FINAL

--- 7.20(4,7)/7.20(4,7)/Program.fs
+++ 7.20(4,7)/7.20(4,7)/Program.fs
@@ -31,7 +31,7 @@ let sortByAverageASCII (strings:'string list) =
 
 
 let isGlasn (x : char) =
-    (x = 'а') || (x = 'и') || (x = 'е') || (x = 'ё') || (x = 'о') || (x = 'у') || (x = 'ы') || (x = 'э') || (x = 'ю') || (x = 'я')
+    (x = 'а') || (x = 'и') || (x = 'е') || (x = 'ё') || (x = 'о') || (x = 'у') || (x = 'ы') || (x = 'э') || (x = 'ю') || (x = 'я') || (x = 'a') || (x = 'e') || (x = 'y') || (x = 'u') || (x = 'i') || (x = 'o')
 
 let counts x y =
     if (isGlasn (snd y)) && (not (isGlasn (fst y))) then x + 1
@@ -45,14 +45,24 @@ let countSoch str =
 let sortBySoch str_list : string list =
     Seq.toList (Seq.sortBy countSoch str_list)
 
+let choose i=
+    match i with 
+    |1 -> sortByAverageASCII
+    |2 -> sortBySoch
+    |_ -> exit(0)
 
 [<EntryPoint>]
 let main argv =
+    Console.WriteLine("Выберите задачу для решения и введите строку: \n Отсортировать строки в указанном порядке")
+    Console.WriteLine("1 В порядке увеличения квадратичного отклонения среднего веса ASCII-кода символа строки от среднего веса ASCII-кода символа первой строки")
+    Console.WriteLine("2 В порядке увеличения разницы между количеством сочетаний «гласная-согласная» и «согласная-гласная» в строке")
+    let i =
+        Convert.ToInt32(Console.ReadLine())
     let n = 
         Convert.ToInt32(Console.ReadLine())
     let stringList = 
         readStringList n
     let newList =
-        sortBySoch stringList
+        choose i stringList
     writeList newList
     0 // return an integer exit code
