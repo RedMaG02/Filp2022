bac0f4ac257241a42a30f09ffda57124b075193f
RedMaG02 <94241600+RedMaG02@users.noreply.github.com>
2022-05-16 14:34:11 +0300

    7.18

---
+++ 7.18/7.18/Program.fs
@@ -0,0 +1,15 @@
+﻿// Learn more about F# at http://docs.microsoft.com/dotnet/fsharp
+
+open System
+
+// Define a function to construct a message to print
+let writeArray array =
+    Array.iter (printf "%d ") array
+
+let arrayDividedBy3 array =
+    Array.filter (fun x -> x % 3 = 0) array
+
+[<EntryPoint>]
+let main argv =
+    [|1; 2; 3; 4; 5; 6; 7; 8; 9; 10; 11; 12|] |> arrayDividedBy3 |> writeArray
+    0 // return an integer exit code
