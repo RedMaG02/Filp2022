06eeda56c22b16f1dc7822f5d034daec560c7039
RedMaG02 <94241600+RedMaG02@users.noreply.github.com>
2022-05-20 12:06:01 +0300

    11.16

--- 11/11.pl
+++ 11/11.pl
@@ -57,9 +57,16 @@ waifu(X):- waifu(Y,X),print(Y),nl,fail.
 grand_ma(X,Y):- parent(X,Z),parent(Z,Y),woman(X).%13
 grand_mas(X):- grand_ma(Y,X),print(Y),nl,fail.
 
-grand_ma_and_son(X,Y):- (man(Y),woman(X),grandma(X,Y));(man(X),woman(Y),grandma(Y,X)).%14
+grand_ma_and_son(X,Y):- (man(Y),woman(X),grand_ma(X,Y));(man(X),woman(Y),grand_ma(Y,X)).%14
 
 mulUp(X,Y):-X<10,Y is X,!.%15
 mulUp(X,Y):-X1 is X div 10,mulUp(X1,Y1),Y is (X mod 10)*Y1,!.
 
+%mulDown(X,Y):- X<10, ANSWER is (X * Y), print(ANSWER),!.
+% mulDown(X,Y):- X1 is (x div 10)< Y1 is (Y * (X mod 10)),
+% mulDown(X1,Y1).
+
+mulDown(X,Y):- mulDown(X,Y,1).
+mulDown(X,Y,Z):- X<10, Y is (X*Z),!.
+mulDown(X,Y,Z):- X>9, X1 is (X div 10),Z1 is (Z * (X mod 10)), mulDown(X1,Y,Z1).
 
