512881c6a3b3918b8b7203688084edea725f097a
RedMaG02 <94241600+RedMaG02@users.noreply.github.com>
2022-05-21 21:01:59 +0300

    12.13

--- 11/12.pl
+++ 11/12.pl
@@ -24,3 +24,19 @@ minDel(X,ANS,Y):- NEWY is Y + 1, minDel(X,ANS1,NEWY), ((X mod Y =:= 0), (Y<ANS1)
 method12(X,ANS):- sumCifr(X,SUM), minDel(X,DEL), method12(X,ANS,SUM,DEL,-1,1).
 method12(X,ANS,SUM,_,MAX,X):- ANS is SUM * MAX,!.
 method12(X,ANS,SUM,DEL,MAX,I):- NEWI is I + 1, nod(X,I,NOD), INDEX is I mod DEL, (((NOD \= 1),(INDEX \= 0),(I > MAX)) -> NEWMAX is I; NEWMAX is MAX ), method12(X,ANS,SUM,DEL,NEWMAX,NEWI).
+
+
+
+prost(X,ANSWER):-DEL is X-1, prost(X,ANSWER,DEL).
+prost(_,ANSWER,1):- ANSWER is 0,!.
+prost(X,ANSWER,DEL):- NEWDEL is DEL -1, prost(X,ANSWER1,NEWDEL),DELEN is X mod DEL, (DELEN =:= 0 -> ANSWER is ANSWER1 + 1; ANSWER is ANSWER1).
+
+polinom(A,B,K):-polinom(A,B,K,0,1).
+polinom(_,_,K,_,0):-K is 1 - 1 ,!.
+polinom(A,B,K,N,B):- NEWN is N + 1, CHISLO is abs((N*N) + (A*N) + B), prost(CHISLO,BOOLK), (BOOLK =:= 0 -> NEWK is K+1,NEWB is 1; NEWK is K, NEWB is 0), polinom(A,B,NEWK,NEWN,NEWB).
+
+%method13(ANSWER,A,B,P,MAX)
+method13(ANSWER):-method13(ANSWER,(-999),(-999),(-1000000),0).
+method13(P,999,1000,P,_):-print(P),!.
+method13(ANSWER,A,1000,P,MAX):- NEWA is A +1, method13(ANSWER,NEWA,(-999),P,MAX).
+method13(ANSWER,A,B,P,MAX):- NEWB is B+1, polinom(A,B,K), (K>MAX -> NEWMAX is K, NEWP is A * B; NEWMAX is MAX, NEWP is P), method13(ANSWER,A,NEWB,NEWP,NEWMAX).
